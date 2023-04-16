ce1a2e6d770327e04bb95a0ac3dfb7ffdaf5484e
bachpx-vbee <bachpx.vbee@gmail.com>
2023-04-16 20:55:35 +0700

    add some necessary file

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import TestComponent from "./components/test";
+
+function App() {
+    return (
+        <div>
+            <h1>Testing</h1>
+        {/* Cần một cái header chung
+        Thêm component vào đây để test */}
+            <TestComponent />
+        </div> 
+    )
+}
+
+export default App;
---
+++ src/components/test.jsx
@@ -0,0 +1,7 @@
+const TestComponent = () => {
+    return (
+        <h1>Test Component</h1>
+    )
+}
+
+export default TestComponent;
