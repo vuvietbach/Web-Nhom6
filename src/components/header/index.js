04519df386f7abd6ce17c0288493724bb7173079
vuvietbach <vuvietbach12345@gmail.com>
2023-04-27 14:31:26 +0700

    gap loi

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,9 +4,15 @@ import SearchIcon from '@mui/icons-material/Search';
 import Divider from '@mui/material/Divider';
 import InputBase from '@mui/material/InputBase';
 import Button from '@mui/material/Button';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import { Box, Typography } from '@mui/material';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import IconButton from '@mui/material/IconButton';
+import Link from '@mui/material/Link';
 export default function Header() {
+    const thucPham = ['trái cây', 'thịt, trứng', 'rau củ quả', 'sữa, bơ, phô mai', 'hải sản', 'gạo, mì ăn liền', 'đồ uống, bia rượu', 'bánh kẹo'];
+    const thucPhamLink = thucPham.map((item, index) => {
+        <Link href='#' underline='none'>{item}</Link>
+    })
   return (
     <div>
         <div className={styles.header}>
@@ -24,8 +30,25 @@ export default function Header() {
                         <Button variant="text" sx={{px: '10px'}}>Tìm kiếm</Button>
                     </div>
                 </div>
-                <FontAwesomeIcon icon={['fas', 'shopping-cart']} style={{marginRight: '5px'}}/>
+                {/* <FontAwesomeIcon icon={['fas', 'shopping-cart']} style={{marginRight: '5px'}}/> */}
+                <Button sx={{height: '100%'}}>
+                    <img src='https://salt.tikicdn.com/ts/upload/32/56/db/d919a4fea46f498b5f4708986d82009d.png' style={{height:'70%', paddingRight:'5px'}}/>
+                    <Typography fontSize={14}>Trang chủ</Typography>
+                </Button> 
+                <Button sx={{height: '100%'}}>
+                    <img src='https://salt.tikicdn.com/ts/upload/41/28/7d/4713aa0d2855c5c770799f248692f0c5.png' style={{height:'70%', paddingRight:'5px'}}/>
+                    <Typography fontSize={14}>Astra</Typography>
+                </Button> 
+                <Button sx={{height: '100%'}}>
+                    <img src='https://salt.tikicdn.com/ts/upload/07/d5/94/d7b6a3bd7d57d37ef6e437aa0de4821b.png' style={{height:'70%', paddingRight:'5px'}}/>
+                    <Typography fontSize={14}>Tài khoản</Typography>
+                </Button> 
+                <Divider orientation="vertical" variant="middle" flexItem sx={{mx: '10px'}}/>
+                <IconButton aria-label="gio hang"><ShoppingCartIcon /></IconButton>
             </div>
+            <Box sx={{display:'flex'}}>
+                {thucPham.map((item, index) => {<span>{item}</span>})}
+            </Box>
 
             {/* <div class="sh1_btn sh1b">
                 <i class="fa-solid fa-house sh1i"></i>
