47884f1dd43ee83f2984b5fb4749ccb22119655c
vuvietbach <vuvietbach12345@gmail.com>
2023-05-24 13:01:26 +0700

    fixing path

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import TestComponent from "./components/test";
-import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Routes,
+} from "react-router-dom";
 import SignUp from "./components/SignUp/SignUp";
 import SignIn from "./components/SignIn/SignIn";
 import OrderDetail from "./components/OrderDetail/Order-Detail";
-function App(){
-    return(
-      <div className="App">
-        <Router>
-          <Routes>
-            <Route path="/" element={<TestComponent/>}/>
-            <Route path="/SignUp" element={<SignUp/>} />
-            <Route path="/SignIn" element={<SignIn/>} />
-            <Route path="/OrderDetail" element={<OrderDetail/>} />
-            
-          </Routes>
-        </Router>
-      </div>
-    );
-  }
+import ProductPage from "pages/productPage/productPage";
+import LandingPage from "pages/landingPage/landingPage";
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/SignIn" element={<SignIn />} />
+          <Route path="/OrderDetail" element={<OrderDetail />} />
+          <Route path="/:product-category" element={<ProductPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
 
 export default App;
