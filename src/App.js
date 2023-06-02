9350d5e447a82db601b104ec92067af4fd6573b4
unknown <you@example.com>
2023-06-02 20:50:15 +0700

    cap nhat trang orderdetail, ghep api dang ki dang nhap, chi tiet san pham

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import TestComponent from "./components/test";
 import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
 import SignUp from "./components/SignUp/SignUp";
 import SignIn from "./components/SignIn/SignIn";
 import OrderDetail from "./components/OrderDetail/Order-Detail";
+import ProductPage from "pages/productPage/productPage";
 function App(){
     return(
       <div className="App">
         <Router>
           <Routes>
-            <Route path="/" element={<TestComponent/>}/>
             <Route path="/SignUp" element={<SignUp/>} />
             <Route path="/SignIn" element={<SignIn/>} />
             <Route path="/OrderDetail" element={<OrderDetail/>} />
+            <Route path="/ProductPage" element={<ProductPage/>} />
             
           </Routes>
         </Router>
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,78 +1,203 @@
-import React, { Component } from 'react'
-import './SignUp.css' 
-import {post, isValidName, isValidPassword, isValidPhoneNumber, isValidConfirmPassword,isPost,isValid} from "./main.js"
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, Navigate,useNavigate, json } from 'react-router-dom';
+import "./SignUp.css"
+import axios from 'axios';
 
-class SignUp extends Component {
-    componentDidMount() {
-        isPost();
-      }
-    componentDidUpdate() {
-        isPost();
+const SignUp = () => {
+  const [isValidPhoneNumber, setIsValidPhoneNumber] = useState(false);
+  const [isValidUsername, setIsValidUsername] = useState(false);
+  const [isValidPassword, setIsValidPassword] = useState(false);
+  const [isValidConfirmPassword, setIsValidConfirmPassword] = useState(false);
+  const [isValidName, setIsValidName] = useState(false);
+  const [isValid, setIsValid] = useState(false);
+  const [phoneNumberInputValue, setPhoneNumberInputValue] = useState('');
+  const [usernameInputValue, setUsernameInputValue] = useState('');
+  const [passwordInputValue, setPasswordInputValue] = useState('');
+  const [confirmPasswordInputValue, setConfirmPasswordInputValue] = useState('');
+  const [nameInputValue, setNameInputValue] = useState('');
+  const [role, setRole] = useState("user");
+  const navigate = useNavigate();
+
+  const checkValidPhoneNumber = (e) => {
+    const input = e.target.value;
+    const len = input.length;
+    setPhoneNumberInputValue(input);
+
+    if (len !== 10) {
+      document.querySelector(".validate-phone-number").innerHTML = "Số điện thoại phải gồm 10 chữ số";
+      setIsValidPhoneNumber(false);
+    } else {
+      document.querySelector(".validate-phone-number").innerHTML = "";
+      setIsValidPhoneNumber(true);
     }
-  render() {
-    return (
-        <div className='body'>
-            <div>
-                <div className="Navbar">
-                    <div className="shop-icon">
-                        <i className="fa-sharp fa-solid fa-shop fa-xl"></i> TEAM6-SHOP
-                    </div>
-                    <div className="sign-up-title">Đăng ký</div>
-                    <a href="" className="help">Bạn cần giúp đỡ?</a>
-                </div>
-    
-                <div className="main">
-                    <form action="" className="sign-up-form">
-                        <div>
-                            <div className="sign-up-subtitle">
-                                Đăng ký  
-                            </div>
-    
-                            <input type="number" placeholder="Số điện thoại" className="input-box phone-number-input" onChange={() => {
-                                isValidPhoneNumber();
-                                isPost();
-                            }}/>
-                            <span className="validate-phone-number"></span>
-    
-                            <input type="text" placeholder="Tên tài khoản" className="input-box name-input" onChange={() => {
-                                isValidName();
-                                isPost();
-                            }}/>
-                            <span className="validate-account-name"></span>
-    
-                            <input type="password" placeholder="Mật khẩu" className="input-box password-input" onChange={() => {
-                                isValidPassword();
-                                isPost();
-                            }} />
-                            <span className="validate-password"></span>
-    
-                            <input type="password" placeholder="Xác nhận mật khẩu" className="input-box confirm-password" onChange={() => {
-                                isValidConfirmPassword();
-                                isPost();
-                            }}/>
-                            <span className="validate-confirm-password"></span>
+  };
 
-                            <select id="role">
-                                <option value="user">User</option>
-                                <option value="seller">Seller</option>
-                            </select>
-    
-                            <button className="post-btn" id="post" onClick={post}>TIẾP THEO</button>
-                            <div className="rules">
-                                Bằng việc đăng kí, bạn đã đồng ý với Shopee về <a href="http://" target="_blank" rel="noopener noreferrer">Điều khoản dịch vụ </a>
-                                & <a href="http://" target="_blank" rel="noopener noreferrer">Chính sách bảo mật</a>                
-                            </div>
-                            <div className="sign-in">
-                                Bạn đã có tài khoản? <a href="http://" target="_blank" rel="noopener noreferrer" style={{ opacity: 1 }}><Link to="/SignIn">Đăng nhập</Link></a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-      );
+  const checkValidUsername = (e) => {
+    const username = e.target.value;
+    setUsernameInputValue(username);
+
+    if (username === "") {
+      document.querySelector(".validate-account-name").innerHTML = "Vui lòng nhập tên tài khoản";
+      setIsValidUsername(false);
+    } else {
+      document.querySelector(".validate-account-name").innerHTML = "";
+      setIsValidUsername(true);
+    }
+  };
+
+  const checkValidName = (e) => {
+    const name = e.target.value;
+    setNameInputValue(name);
+
+    if (name === "") {
+      document.querySelector(".validate-name").innerHTML = "Vui lòng nhập tên người dùng";
+      setIsValidName(false);
+    } else {
+      document.querySelector(".validate-name").innerHTML = "";
+      setIsValidName(true);
+    }
+  };
+
+  const checkValidPassword = (e) => {
+    const input = e.target.value;
+    setPasswordInputValue(input);
+
+    if (input.length < 8 || input.length > 15) {
+      document.querySelector(".validate-password").innerHTML = "Vui lòng nhập mật khẩu từ 8-15 kí tự";
+      setIsValidPassword(false);
+    } else {
+      document.querySelector(".validate-password").innerHTML = "";
+      setIsValidPassword(true);
+    }
+  };
+
+  const checkValidConfirmPassword = (e) => {
+    const input = e.target.value;
+    setConfirmPasswordInputValue(input);
+  
+    if (input.length < 8 || input.length > 15) {
+      document.querySelector(".validate-confirm-password").innerHTML = "Vui lòng nhập mật khẩu từ 8-15 kí tự";
+      setIsValidConfirmPassword(false);
+    } else if (input !== passwordInputValue) {
+      document.querySelector(".validate-confirm-password").innerHTML = "Vui lòng nhập đúng mật khẩu";
+      setIsValidConfirmPassword(false);
+    } else {
+      document.querySelector(".validate-confirm-password").innerHTML = "";
+      setIsValidConfirmPassword(true);
+    }
+  };
+  
+  const post = (e) => {
+    e.preventDefault();
+    if (!isValidName || !isValidPassword || !isValidConfirmPassword || !isValidPhoneNumber) {
+      // Xử lý khi dữ liệu không hợp lệ
+    } else {
+      const data = {
+        username: usernameInputValue,
+        password: passwordInputValue,
+        name: nameInputValue,
+        phone_number: phoneNumberInputValue
+      };
+      const url = role === "user" ? "http://localhost:8080/user/create-user" : "http://localhost:8080/seller/create-seller";
+  
+      axios.post(url, data)
+        .then(response => {
+          console.log(response.data);
+          document.querySelector(".error-message").innerHTML = "";
+          alert("Tạo tài khoản thành công");
+          navigate("/SignIn");
+          // localStorage.setItem("user",JSON.stringify(response.data.data));
+        })
+        .catch(error => {
+          if (error.response && error.response.data && error.response.data.message) {
+            const errorMessage = error.response.data.message;
+            console.log(errorMessage);
+            console.log(error.response.data?.status);
+            document.querySelector(".error-message").innerHTML = errorMessage;
+          } else {
+            document.querySelector(".error-message").innerHTML = "Tài khoản này đã tồn tại";
+          }
+        });
+    }
+  };
+  
+  
+
+  const isPost = () => {
+    const isValid = isValidUsername && isValidPassword && isValidConfirmPassword && isValidPhoneNumber && isValidName;
+    setIsValid(isValid);
+
+    if (!isValid) {
+      document.getElementById("post").disabled = true;
+      document.getElementById("post").style.cursor = 'no-drop';
+    } else {
+      document.getElementById("post").disabled = false;
+      document.getElementById("post").style.cursor = 'pointer';
+    }
+  };
+
+  useEffect(() => {
+    isPost();
+  }, [isValidPhoneNumber, isValidUsername, isValidPassword, isValidConfirmPassword, isValidName]);
+
+  const selectRole = (e) => {
+    setRole(e.target.value);
+    console.log(role);
   }
-}
 
-export default SignUp
+  return (
+    <div className='body'>
+      <div>
+        <div className="Navbar">
+          <div className="shop-icon">
+            <i className="fa-sharp fa-solid fa-shop fa-xl"></i> TEAM6-SHOP
+          </div>
+          <div className="sign-up-title">Đăng ký</div>
+          <a href="" className="help">Bạn cần giúp đỡ?</a>
+        </div>
+
+        <div className="main">
+          <form action="" className="sign-up-form">
+            <div>
+              <div className="sign-up-subtitle">
+                Đăng ký
+              </div>
+
+              <input type="number" placeholder="Số điện thoại" className="input-box phone-number-input" onChange={checkValidPhoneNumber} />
+              <span className="validate-phone-number error"></span>
+
+              <input type="text" placeholder="Tên người dùng" className="input-box name-input" onChange={checkValidName} />
+              <span className="validate-name error"></span>
+
+              <input type="text" placeholder="Tên tài khoản" className="input-box username-input" onChange={checkValidUsername} />
+              <span className="validate-account-name error"></span>
+
+              <input type="password" placeholder="Mật khẩu" className="input-box password-input" onChange={checkValidPassword} />
+              <span className="validate-password error"></span>
+
+              <input type="password" placeholder="Xác nhận mật khẩu" className="input-box confirm-password" onChange={checkValidConfirmPassword} />
+              <span className="validate-confirm-password error"></span>
+
+              <select id="role" onChange={selectRole}>
+                <option value="user">User</option>
+                <option value="seller">Seller</option>
+              </select>
+
+              <button className="post-btn" id="post" onClick={post}>TIẾP THEO</button>
+              <span className="error-message error"></span>
+              <div className="rules">
+                Bằng việc đăng kí, bạn đã đồng ý về <a href="http://" target="_blank" rel="noopener noreferrer">Điều khoản dịch vụ </a>
+                & <a href="http://" target="_blank" rel="noopener noreferrer">Chính sách bảo mật</a>
+              </div>
+              <div className="sign-in">
+                Bạn đã có tài khoản? <Link to="/SignIn">Đăng nhập</Link>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
