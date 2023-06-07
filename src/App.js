42db7effa8463a4dbd6b46a23718b210d70a1a9b
unknown <you@example.com>
2023-06-08 00:43:34 +0700

    Add send refreshToken

--- src/App.js
+++ src/App.js
@@ -7,27 +7,30 @@ import {
 import SignUp from "./components/SignUp/SignUp";
 import SignIn from "./components/SignIn/SignIn";
 import OrderDetail from "./components/OrderDetail/Order-Detail";
-import Account from 'components/Account_info/Account';
+import Account from "components/Account_info/Account";
 import LandingPage from "pages/landingPage/landingPage";
 import ProductPage from "pages/productPage/productPage";
-import GioHang from "./components/GioHang/GioHang"
+import GioHang from "./components/GioHang/GioHang";
+import { CookiesProvider } from "react-cookie";
 
-function App(){
-    return(
+function App() {
+  return (
+    <CookiesProvider>
       <div className="App">
         <Router>
           <Routes>
-            <Route path="/" element={<LandingPage/>}/>
-            <Route path="/SignUp" element={<SignUp/>} />
-            <Route path="/SignIn" element={<SignIn/>} />
-            <Route path="/Account" element={<Account/>} />
-            <Route path="/chi-tiet-san-pham" element={<OrderDetail/>} />
-            <Route path="/GioHang" element={<GioHang/>}/>
-            <Route path="/danh-muc/:id" element={<ProductPage/>}/>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/SignUp" element={<SignUp />} />
+            <Route path="/SignIn" element={<SignIn />} />
+            <Route path="/Account" element={<Account />} />
+            <Route path="/chi-tiet-san-pham" element={<OrderDetail />} />
+            <Route path="/GioHang" element={<GioHang />} />
+            <Route path="/danh-muc/:id" element={<ProductPage />} />
           </Routes>
         </Router>
       </div>
-    );
-  }
+    </CookiesProvider>
+  );
+}
 
 export default App;
